9512db47a87427404b47df8ad2b7f9f2cb5c8ed8
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-05-29 17:02:02 +0200

    feat: #2 add the base for a register page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./HomePage/home/home.component";
+import {RegisterFormComponent} from "./auth/register-form/register-form.component";
 
 export const routes: Routes = [
   {path: '', component:HomeComponent},
+  {path:'register', component:RegisterFormComponent},
 ];
