463d39e66f16a31909de71fd075eb353c6b04ff8
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-05-30 20:38:18 +0200

    feat: #7 add logout message and logic

--- src/app/logedIn/start-screen/start-screen.component.ts
+++ src/app/logedIn/start-screen/start-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {LogoutService} from "../../auth/logout.service";
 
 
 @Component({
@@ -9,4 +10,29 @@ import { Component } from '@angular/core';
   styleUrl: './start-screen.component.css'
 })
 export class StartScreenComponent {
+  logoutMessage: string | null = null;
+
+  constructor(private logoutStatusService: LogoutService) {
+    this.observeLogoutStatus();
+  }
+
+  observeLogoutStatus() {
+    this.logoutStatusService.currentStatus.subscribe(status => {
+      if (status) {
+        let counter = 8;
+        this.logoutMessage = `Succesfully signed out. Returning to homepage in ${counter} seconds...`;
+
+        const intervalId = setInterval(() => {
+          counter--;
+          if (counter >= 0) {
+            this.logoutMessage = `Succesfully signed out. Returning to homepage in ${counter} seconds...`;
+          } else {
+            this.logoutMessage = null;
+            this.logoutStatusService.changeStatus(false);
+            clearInterval(intervalId);
+          }
+        }, 1000);
+      }
+    });
+  }
 }
--- src/app/ui/navbar/navbar.component.ts
+++ src/app/ui/navbar/navbar.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
+import {AuthService} from "../../auth/auth.service";
+import {LogoutService} from "../../auth/logout.service";
+
+class LogoutStatusService {
+}
 
 @Component({
   selector: 'app-navbar',
@@ -11,5 +16,14 @@ import {RouterLink} from "@angular/router";
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(protected authService: AuthService, private router: Router, private logoutStatusService: LogoutService) {}
 
+  LogoutClick(): void{
+    this.authService.logout();
+    this.logoutStatusService.changeStatus(true);
+    setTimeout(() => {
+      this.router.navigate(['/']);
+      this.logoutStatusService.changeStatus(false);
+    }, 8000);
+  }
 }
