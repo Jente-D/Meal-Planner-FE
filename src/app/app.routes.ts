4e01a7a604082694239dc0f0d9172acbd0874156
Jente.dorssemont <jente.dorssemont@gmail.com>
2024-06-11 11:17:28 +0200

    feat: #23 adjust nesecery details and make the links voor meallist

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from "./HomePage/home/home.component";
+import {HomeComponent} from "./homePage/home/home.component";
 import {RegisterFormComponent} from "./auth/register-form/register-form.component";
 import {LoginFormComponent} from "./auth/login-form/login-form.component";
 import {StartScreenComponent} from "./logedIn/start-screen/start-screen.component";
+import {MenuItemsComponent} from "./menu/menu-items/menu-items.component";
 
 
 export const routes: Routes = [
@@ -10,4 +11,5 @@ export const routes: Routes = [
   {path:'register', component:RegisterFormComponent},
   {path: 'login', component:LoginFormComponent},
   {path:'welcome', component:StartScreenComponent},
+  {path:'meals', component:MenuItemsComponent},
 ];
